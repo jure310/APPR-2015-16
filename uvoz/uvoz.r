d72a4db0af0dc71923df46464bb11b5360d3c4ac
Jure Cankar <jcankar159@gmail.com>
2015-12-15 15:54:48 +0100

    Dodal se tabelo za povprčno število otrok v razredu v državah OECD

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -13,3 +13,17 @@ colnames(razmerje)[2] <- "Vrsta sole"
 colnames(razmerje)[3] <- "Primarna stopnja izobrazbe"
 colnames(razmerje)[4] <- "Sekundarna stopnja izobrazbe"
 
+uvozi.povprecje <- function() {
+  return(read.table("podatki/povprecen_razred.csv", sep = ";", as.is = TRUE,
+                    fileEncoding = "Windows-1250"))
+}
+
+
+povprecje <- uvozi.povprecje()
+povprecje <- povprecje[-3]
+
+colnames(povprecje)[1] <- "Drzava"
+colnames(povprecje)[2] <- "Vrsta sole"
+colnames(povprecje)[3] <- "Primarna stopnja izobrazbe"
+colnames(povprecje)[4] <- "Sekundarna stopnja izobrazbe"
+
